3043ae932c41c13b928eed752e131f1891b92c1a
김정희 <ealiyel@naver.com>
2023-07-06 21:21:07 +0900

    페이징 처리2

---
+++ HRSYSTEM/src/main/java/com/gjh/hrsystem/common/vo/DefaultVO.java
@@ -0,0 +1,132 @@
+package com.gjh.hrsystem.common.vo;
+
+public class DefaultVO {
+
+	private int no; 				// 번호
+	private int end; 				// 검색할 마지막 번호
+	private int start; 				// 검색할 시작 번호
+	private int rnum; 				// 검색 되었을 때의 몇번째 번호인가 확인하는 번호
+	private int currentPageNo; 		// 현재 페이지 No
+	private int totalRecordCount; 	// 전체 게시물 건 수
+	private int totalPageCount; 	// 총 페이지의 갯수
+	private int recordCountPerPage; // 페이지당 게시되는 게시물 건 수
+	private int pageSize; 			// 페이지 리스트에 게시되는 페이지 건수
+	private String sortName; 		// 정렬 대상 컬럼
+	private String sortType; 		// 정렬 방식
+	private String regDate; 		// 등록일
+	private String modDate; 		// 수정일
+	private String procFlag; 		// add,mod,del 구분값(추가, 수정, 삭제)
+
+	
+	public String getProcFlag() {
+		return procFlag;
+	}
+
+	public void setProcFlag(String procFlag) {
+		this.procFlag = procFlag;
+	}
+
+	public String getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(String modDate) {
+		this.modDate = modDate;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+
+	public void setEnd(int end) {
+		this.end = end;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setStart(int start) {
+		this.start = start;
+	}
+
+	public int getRnum() {
+		return rnum;
+	}
+
+	public void setRnum(int rnum) {
+		this.rnum = rnum;
+	}
+
+	public int getCurrentPageNo() {
+		return currentPageNo;
+	}
+
+	public void setCurrentPageNo(int currentPageNo) {
+		this.currentPageNo = currentPageNo;
+	}
+
+	public int getTotalRecordCount() {
+		return totalRecordCount;
+	}
+
+	public void setTotalRecordCount(int totalRecordCount) {
+		this.totalRecordCount = totalRecordCount;
+	}
+
+	public int getTotalPageCount() {
+		return totalPageCount;
+	}
+
+	public void setTotalPageCount(int totalPageCount) {
+		this.totalPageCount = totalPageCount;
+	}
+
+	public int getRecordCountPerPage() {
+		return recordCountPerPage;
+	}
+
+	public void setRecordCountPerPage(int recordCountPerPage) {
+		this.recordCountPerPage = recordCountPerPage;
+	}
+
+	public int getPageSize() {
+		return pageSize;
+	}
+
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+
+	public String getSortName() {
+		return sortName;
+	}
+
+	public void setSortName(String sortName) {
+		this.sortName = sortName;
+	}
+
+	public String getSortType() {
+		return sortType;
+	}
+
+	public void setSortType(String sortType) {
+		this.sortType = sortType;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+}
---
+++ HRSYSTEM/src/main/webapp/WEB-INF/views/template/user/leftSide.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
+<%@ page trimDirectiveWhitespaces="true"%>
+
+<div class="col-md-2">
+	<h4 class="sidebar-title">왼쪽 사이드바 메뉴</h4>
+   	<ul class="nav nav-sidebar">
+    	<li><a href="#">메뉴 1</a></li>
+        <li><a href="#">메뉴 2</a></li>
+       	<li><a href="#">메뉴 3</a></li>
+        <li><a href="#">메뉴 4</a></li>
+    </ul>
+</div>
